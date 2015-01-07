6642328e53188a48bf109fcd327fe9dbdeb3fd90
axtscz <axtscz@gmail.com>
2015-01-07 08:54:26 -0600

    Commit 1: Basis of project. A few assets and sprite behaviors.

---
+++ Assets/Standard Assets/Scripts/Controller.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class Controller : MonoBehaviour
+{
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector2 speed = new Vector2(50, 50);
+	
+	// 2 - Store the movement
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 4 - Movement per direction
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+
+		if (Input.GetKeyUp (KeyCode.X)) {
+			rigidbody2D.MoveRotation(10);
+			print("Hey!");
+				}
+
+	}
+	
+	void FixedUpdate()
+	{
+		// 5 - Move the game object
+		rigidbody2D.velocity = movement;
+
+	}
+}
---
+++ Assets/Standard Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public Transform other;
+	void Update() {
+		if (other) {
+			Vector2 forward = transform.TransformDirection(Vector3.forward);
+			Vector2 toOther = other.position - transform.position;
+			if (Vector2.Dot(forward, toOther) < 0)
+				print("The other transform is behind me!");
+			
+		}
+	}
+}
